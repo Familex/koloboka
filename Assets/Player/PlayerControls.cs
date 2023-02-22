ba69b2e0fa961c22e591c7bc63a2284aa94b3e64
Familex <trener2990@gmail.com>
2023-02-22 20:43:46 +0300

    third person camera

--- Assets/Player/PlayerControls.cs
+++ Assets/Player/PlayerControls.cs
@@ -4,40 +4,55 @@ namespace Player
 {
     public class PlayerControls : MonoBehaviour
     {
+        [SerializeField] private Camera firstPersonView;
+        [SerializeField] private Camera thirdPersonView;
+        
         private Rigidbody _rigidbody;
         private const float Speed = 3;
         
         #region Keys
-        [SerializeField] private KeyCode forward_key = KeyCode.W;
-        [SerializeField] private KeyCode backward_key = KeyCode.S;
-        [SerializeField] private KeyCode left_key = KeyCode.A;
-        [SerializeField] private KeyCode right_key = KeyCode.D;
+        [SerializeField] private KeyCode forwardKey = KeyCode.W;
+        [SerializeField] private KeyCode backwardKey = KeyCode.S;
+        [SerializeField] private KeyCode leftKey = KeyCode.A;
+        [SerializeField] private KeyCode rightKey = KeyCode.D;
         #endregion
         
         // Start is called before the first frame update
         private void Start()
         {
+            firstPersonView.enabled = true;
+            thirdPersonView.enabled = false;
             _rigidbody = GetComponent<Rigidbody>();
         }
 
         // Update is called once per frame
         private void Update()
         {
-            if (Input.GetKey(forward_key))
+            if (Input.GetKey(forwardKey))
             {
                 _rigidbody.AddForce(0, 0, Speed);
             }
-            if (Input.GetKey(left_key))
+            if (Input.GetKey(leftKey))
             {
                 _rigidbody.AddForce(-Speed, 0, 1);
             }
-            if (Input.GetKey(right_key))
+            if (Input.GetKey(rightKey))
             {
                 _rigidbody.AddForce(Speed, 0, 0);
             }
-            if (Input.GetKey(backward_key))
+            if (Input.GetKey(backwardKey))
             {
                 _rigidbody.AddForce(0, 0, -Speed);
+            }
+
+            if (Input.GetKey(KeyCode.Alpha1))
+            {
+                firstPersonView.enabled = true;
+                thirdPersonView.enabled = false;
+            } else if (Input.GetKey(KeyCode.Alpha2))
+            {
+                firstPersonView.enabled = false;
+                thirdPersonView.enabled = true;
             }
         }
     }
