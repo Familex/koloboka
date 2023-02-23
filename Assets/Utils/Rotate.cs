4a7899347ead12025fd1b09874cdf3c27aa6cbea
Familex <trener2990@gmail.com>
2023-02-23 23:16:12 +0300

    test scene + lightning config

--- Assets/Utils/Rotate.cs
+++ Assets/Utils/Rotate.cs
@@ -7,7 +7,6 @@ namespace Utils
         [SerializeField] private float speed = 1;
         [SerializeField] private Vector3 direction = new(0, 1, 0);
 
-        // Update is called once per frame
         private void FixedUpdate()
         {
             transform.Rotate(direction.normalized * (speed * Time.deltaTime));
