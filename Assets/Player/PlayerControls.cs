460b04af0dd261e3eab7d18b9171ea531730420b
Familex <trener2990@gmail.com>
2023-02-25 00:58:57 +0300

    all input switched to NEW unity input system but it throws 999+ errors while run

--- Assets/Player/PlayerControls.cs
+++ Assets/Player/PlayerControls.cs
@@ -5,31 +5,67 @@ namespace Player
     public class PlayerControls : MonoBehaviour
     {
         [SerializeField] private float force = 10;
-        [SerializeField] private float rotationSpeed = 1;
+        [SerializeField] private float rotationSpeed = 15;
+        [SerializeField] private Camera[] cameras;
+        [SerializeField] private Rigidbody playerRigidbody;
         
-        private Rigidbody _rigidbody;
+        private ControlActions _controlActions;
+        private int _currentCamera;
+
+        private void TurnOnCurrent()
+        {
+            foreach (var cam in cameras)
+            {
+                cam.enabled = false;
+            }
+
+            cameras[_currentCamera].enabled = true;
+        }
         
-        #region Keys
-        [SerializeField] private KeyCode forwardKey = KeyCode.W;
-        [SerializeField] private KeyCode backwardKey = KeyCode.S;
-        [SerializeField] private KeyCode leftKey = KeyCode.A;
-        [SerializeField] private KeyCode rightKey = KeyCode.D;
-        #endregion
-        
+        private void Awake()
+        {
+            _controlActions = new ControlActions();
+
+            if (cameras.Length > 0)
+            {
+                _controlActions.debug.camerachange.performed += ctx =>
+                {
+                    _currentCamera += Mathf.RoundToInt(ctx.ReadValue<float>());
+                    if (_currentCamera < 0) _currentCamera = cameras.Length - 1;
+                    if (_currentCamera >= cameras.Length) _currentCamera = 0;
+                    TurnOnCurrent();
+                };
+            }
+        }
+
+        public void OnEnable()
+        {
+            _controlActions.Enable();
+        }
+
+        public void OnDisable()
+        {
+            _controlActions.Disable();
+        }
+
         private void Start()
         {
-            _rigidbody = GetComponent<Rigidbody>();
+            if (cameras.Length > 0)
+            {
+                _currentCamera = 0;
+                TurnOnCurrent();
+            }
         }
 
         private void Update()
         {
-            Debug.Log(_rigidbody.rotation.eulerAngles);
+            // Debug.Log(_rigidbody.rotation.eulerAngles);
             
             // Move and rotate
             {
                 var deltaForce = new Vector3();
                 var deltaRotate = new Vector3();
-                var rotation = transform.rotation;
+                var rotation = playerRigidbody.transform.rotation;
                 var rotationX = rotation.x;
                 var rotationY = rotation.y;
                 var forward = new Vector3(
@@ -37,33 +73,18 @@ namespace Player
                     0,
                     Mathf.Cos(rotationY + rotationX)
                 );
-                
-                if (Input.GetKey(forwardKey))
-                {
-                    deltaForce += forward;
-                }
 
-                if (Input.GetKey(backwardKey))
-                {
-                    deltaForce -= forward;
-                }
+                var dPad = _controlActions.gameplay.move.ReadValue<Vector2>();
 
-                if (Input.GetKey(leftKey))
-                {
-                    deltaRotate.y -= Time.deltaTime * rotationSpeed;
-                }
+                deltaForce += forward * dPad.y;
+                deltaRotate.y += dPad.x * rotationSpeed;
 
-                if (Input.GetKey(rightKey))
-                {
-                    deltaRotate.y += Time.deltaTime * rotationSpeed;
-                }
-                
                 deltaForce *= force;
 
-                Debug.DrawRay(transform.position, deltaForce, Color.magenta);
+                Debug.DrawRay(playerRigidbody.transform.position, deltaForce, Color.magenta);
                 
-                _rigidbody.AddForce(deltaForce);
-                _rigidbody.transform.Rotate(deltaRotate);
+                playerRigidbody.AddForce(deltaForce);
+                playerRigidbody.transform.Rotate(deltaRotate);
             }
         }
     }
--- Assets/Utils/CameraViewChange.cs
+++ Assets/Utils/CameraViewChange.cs
@@ -1,51 +1,11 @@
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 namespace Utils
 {
-    [System.Serializable]
-    public class CamSwitch
-    {
-        [SerializeField] public Camera cam;
-        [SerializeField] public KeyCode key;
-            
-        public void Deconstruct(out Camera camInner, out KeyCode keyInner)
-        {
-            camInner = cam;
-            keyInner = key;
-        }
-    }
-    
     public class CameraViewChange : MonoBehaviour
     {
-        [SerializeField] private CamSwitch[] cameras;
         
-        private void OffAll()
-        {
-            foreach (var cam in cameras)
-            {
-                cam.cam.enabled = false;
-            }
-        }
-       
-        #if UNITY_EDITOR
-            private void Start()
-            {
-                OffAll();
-                if (cameras.Length > 0)
-                {
-                    cameras[0].cam.enabled = true;
-                }
-            }
-
-            private void Update()
-            {
-                foreach (var (cam, key) in cameras)
-                {
-                    if (!Input.GetKey(key)) continue;
-                    OffAll();
-                    cam.enabled = true;
-                }
-            }
-        #endif
     }
 }
