b1bdaad3f9d178847011eb8d2c887ea4d2f43562
Familex <trener2990@gmail.com>
2023-02-25 15:20:01 +0300

    fix errors in Main scene, add simple navMesh agents (prefab in Enemy folder), minor changes

---
+++ Assets/Enemy/MoveTo.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy {
+    [RequireComponent(typeof(NavMeshAgent))]
+    public class MoveTo : MonoBehaviour {
+        /* ---- Inspector things ---- */
+        [SerializeField] private Transform goal;
+        
+        /* ---- Private constants ---- */
+        private NavMeshAgent _navMeshAgent;
+        
+        /* ---- Unity overrides ---- */
+        private void Start()
+        {
+            _navMeshAgent = GetComponent<NavMeshAgent>();
+        }
+
+        private void Update () {
+            _navMeshAgent.destination = goal.position; 
+        }
+    }
+}
